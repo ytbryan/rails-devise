9bb8c7d342fd2b36e0be81ddfc0457b38351077b
ytbryan <ytbryan@gmail.com>
2018-03-28 21:57:05 +0800

    a set of application helpers for embeddable markdown and pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def emd content
+    Markdown.new(content).to_html.html_safe
+  end
+
+  def calculate_index page, index, limit
+    if page
+      (limit * (page.to_i - 1 )) + (index +1)
+    else 
+      index +1
+    end
+  end
 end
