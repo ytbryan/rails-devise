2d104bc09792c4a5cac437d3051e6dfe1f4a48c7
ytbryan <ytbryan@gmail.com>
2018-03-29 22:55:03 +0800

    added omniauthable to user.rb and from_omniauth. Create user if he/she does not exist

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+  devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :questions
-  has_many :answers         
+  has_many :answers    
+  
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # When user does not exist
+    unless user
+        user = User.create(name: data['name'],
+           email: data['email'],
+           password: Devise.friendly_token[0,20]
+        )
+    end
+
+    user
+end
+
 end
