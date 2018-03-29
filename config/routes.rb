cf98c65ccf0acfef8ca461734ebc697559cd3f8e
ytbryan <ytbryan@gmail.com>
2018-03-29 22:56:05 +0800

    set the devise route to use omni_callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'questions#index'
   get :find, to: "searches#find"
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'omni_callbacks' }
   resources :searches, :answers, :questions, :users
 end
