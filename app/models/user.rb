a4ca0ce2ed393f9838a86baeaf0b527652f1d639
ytbryan <ytbryan@gmail.com>
2018-03-30 00:05:52 +0800

    ✂️✂️✂️ and authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,26 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  
+          :recoverable, :rememberable, :trackable, :validatable
+
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :questions
   has_many :answers    
-  
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    user = User.where(email: data['email']).first
 
+  def self.from_omniauth(access_token)
+    user = User.where(email: access_token.info['email']).first
     # When user does not exist
     unless user
         user = User.create(name: data['name'],
-           email: data['email'],
-           password: Devise.friendly_token[0,20]
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
         )
     end
-
-    user
-end
-
+    return user
+  end
 end
