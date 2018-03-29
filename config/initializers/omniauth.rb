03814e18732352fefef4760fe03ae107ffd89c6a
ytbryan <ytbryan@gmail.com>
2018-03-29 22:56:32 +0800

    configure omniauth.rb as initialiser

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,8 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+    {
+      scope: 'email, profile',
+      access_type: 'offline',
+      provider_ignores_state: true
+    }
+end
